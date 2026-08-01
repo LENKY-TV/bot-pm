6521b60f2418006df0c3a3ed181bdd91653b34c4
LENKY-TV <lenny84800@gmail.com>
2026-08-01 16:05:27 +0200

    Improve /messagepm with embed style

--- commands/messagepm.js
+++ commands/messagepm.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,9 +11,19 @@ module.exports = {
                 .addChannelTypes(ChannelType.GuildText)
         )
         .addStringOption(option =>
-            option.setName('message')
-                .setDescription('Message à envoyer')
+            option.setName('titre')
+                .setDescription('Titre du message')
                 .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('message')
+                .setDescription('Contenu du message')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('couleur')
+                .setDescription('Couleur hex (ex: #FF0000)')
+                .setRequired(false)
         )
         .addUserOption(option =>
             option.setName('ping1')
@@ -39,21 +49,32 @@ module.exports = {
 
     async execute(interaction, client) {
         const channel = interaction.options.getChannel('salon');
+        const titre = interaction.options.getString('titre');
         const message = interaction.options.getString('message');
+        const couleur = interaction.options.getString('couleur') || '#1a1a2e';
         const ping1 = interaction.options.getUser('ping1');
         const ping2 = interaction.options.getUser('ping2');
         const ping3 = interaction.options.getUser('ping3');
         const role = interaction.options.getRole('role');
 
-        let finalMessage = message;
+        let pings = '';
+        if (role) pings += `<@&${role.id}> `;
+        if (ping1) pings += `<@${ping1.id}> `;
+        if (ping2) pings += `<@${ping2.id}> `;
+        if (ping3) pings += `<@${ping3.id}> `;
 
-        if (ping1) finalMessage = `<@${ping1.id}> ${finalMessage}`;
-        if (ping2) finalMessage = `<@${ping2.id}> ${finalMessage}`;
-        if (ping3) finalMessage = `<@${ping3.id}> ${finalMessage}`;
-        if (role) finalMessage = `<@&${role.id}> ${finalMessage}`;
+        const embed = new EmbedBuilder()
+            .setTitle(titre)
+            .setDescription(message)
+            .setColor(couleur)
+            .setFooter({ text: `${interaction.guild.name} RH` })
+            .setTimestamp();
 
         try {
-            await channel.send(finalMessage);
+            await channel.send({
+                content: pings || null,
+                embeds: [embed]
+            });
 
             await interaction.reply({
                 content: `✅ Message envoyé dans ${channel} !`,
